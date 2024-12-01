9d653e127848f6540e937897f73b0fcf5a7fb863
asliddin <aslnabiev2002@gmail.com>
2024-12-01 21:06:28 +0500

    update project files

--- auth/jwt_router.py
+++ auth/jwt_router.py
@@ -1,19 +1,21 @@
 from fastapi import APIRouter,Depends,HTTPException,status
 from ..usersdir.schema import UserLogin,Token
-from ..database import create_db
+from ..main import pool
 from.jwt_handler import hash_pswd,authenticate_user,create_access_token,get_current_user
 
 router=APIRouter(prefix="/api",tags={"auth"})
 
 @router.post("/sign-up")
-async def sign_up(user:UserLogin,conn=Depends(create_db)):
-    hash_password=hash_pswd(user.password)
-    await conn.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (user.username, hash_password))
-    return {"message":"user add successful"}
+async def sign_up(user:UserLogin):
+    async with pool.connection()as conn:
+        async with conn.cursor() as curs:
+            hash_password=hash_pswd(user.password)
+            await curs.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (user.username, hash_password))
+            return {"message":"user add successful"}
     
 @router.post("/sign-in",response_model=Token)
-async def sign_in(user:UserLogin,conn=Depends(create_db)):
-    db_user=await authenticate_user(user.username,user.password,conn)
+async def sign_in(user:UserLogin):
+    db_user=await authenticate_user(user.username,user.password)
     if not db_user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid or expired token")
     access_token=create_access_token("sub",user.username,user.role)
--- bookdir/book_queries.py
+++ bookdir/book_queries.py
@@ -1,60 +1,78 @@
 from fastapi import HTTPException,Depends,status
 from fastapi.responses import JSONResponse
 from .schema import DeleteBook,CreateBook,ResponseBook,PublishedBook,Book
-from ..database import create_db
+from ..main import pool
 
 
-async def get_books(book:Book,db=Depends(create_db)):
-    books= await db.execute("SELECT b.title,b.description, u.username, b.comment,b.janr FROM book as b JOIN users as u ON b.author_id=u.id WHERE b.isresponse=%s and b.ispublished=%s ",(book.isresponse,book.ispublished))
-    if not books:
-        raise HTTPException(status_code=404,detail="books not found")
-    return {"books":books}
-async def get_book_by_id(id:int, book:Book,db=Depends(create_db)):
-    books= await db.execute("SELECT b.title,b.description, u.username, b.comment,b.janr FROM book as b JOIN users as u ON b.author_id=u.id WHERE b.isresponse=%s and b.ispublished=%s ",(book.isresponse,book.ispublished))
-    if not books:
-        raise HTTPException(status_code=404,detail="book`s by id not found")
-    return {"book":books}
-async def create_books(book:CreateBook,db=Depends(create_db)):
+async def get_books(book:Book):
+    async with pool.connection() as conn:
+        async with conn.cursor() as curs:
+            books= await curs.execute("SELECT b.title,b.description, u.username, b.comment,b.janr FROM book as b JOIN users as u ON b.author_id=u.id WHERE b.isresponse=%s and b.ispublished=%s ",(book.isresponse,book.ispublished))
+            if not books:
+                raise HTTPException(status_code=404,detail="books not found")
+            return {"books":books}
+        
+async def get_book_by_id(id:int, book:Book):
+    async with pool.connection() as conn:
+        async with conn.cursor() as curs:
+            books= await curs.execute("SELECT b.title,b.description, u.username, b.comment,b.janr FROM book as b JOIN users as u ON b.author_id=u.id WHERE b.isresponse=%s and b.ispublished=%s ",(book.isresponse,book.ispublished))
+            if not books:
+                raise HTTPException(status_code=404,detail="book`s by id not found")
+            return {"book":books}
+
+async def create_books(book:CreateBook):
     try:
-        books=await db.execute("INSERT into books(title,description,author_id,comment,janr) VALUES(%s,%s,%s,%s)",(book.title,book.description,book.author_id,book.comment,book.janr))
-        if not books:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Bad request")
-        return JSONResponse(content=books)
+        async with pool.connection() as conn:
+            async with conn.cursor() as curs:
+                books=await curs.execute("INSERT into books(title,description,author_id,comment,janr) VALUES(%s,%s,%s,%s)",(book.title,book.description,book.author_id,book.comment,book.janr))
+                if not books:
+                    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Bad request")
+                return JSONResponse(content=books)
     except:
         return {}
     
     
-async def delete_books(book:DeleteBook,id:int,db=Depends(create_db)):
-    books=await db.execute("DELETE from books as b WHERE b.id=%s",(book.id))
-    if not books:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Book id not found")
-    return JSONResponse(content=book)
-async def update_book(book:CreateBook,id:int,db=Depends(create_db)):
+async def delete_books(book:DeleteBook,id:int):
+    async with pool.connection() as conn:
+        async with conn.cursor() as curs:
+            books=await curs.execute("DELETE from books as b WHERE b.id=%s",(book.id))
+            if not books:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Book id not found")
+            return JSONResponse(content=book)
+
+async def update_book(book:CreateBook,id:int):
     try:
-        books=await db.execute("UPDATE books SET title=%s, description=%s,author_id=%s,comment=%s,janr=%s WHERE id=%s",(book.title,book.description,book.author_id,book.comment,book.janr,id))
-        if books:   
-            return JSONResponse(content=books)
-        else:
-            raise HTTPException(status_code=400,detail="bad request")
+        async with pool.connection() as conn:
+            async with conn.cursor() as curs:
+                books=await curs.execute("UPDATE books SET title=%s, description=%s,author_id=%s,comment=%s,janr=%s WHERE id=%s",(book.title,book.description,book.author_id,book.comment,book.janr,id))
+                if books:   
+                    return JSONResponse(content=books)
+                else:
+                    raise HTTPException(status_code=400,detail="bad request")
     except:
         return {}
     
-async def check_as_response(id:int,book:ResponseBook,db=Depends(create_db)):
+async def check_as_response(id:int,book:ResponseBook):
     try:
-        books=await db.execute("UPDATE books SET isresponse=%s WHERE id=%s",(id,book.isresponse))
-        if books:
-            return JSONResponse(content=books)
-        else:
-            raise HTTPException(status_code=400,detail="bad request")
+        async with pool.connection() as conn:
+            async with conn.cursor() as curs:
+                books=await curs.execute("UPDATE books SET isresponse=%s WHERE id=%s",(id,book.isresponse))
+                if books:
+                    return JSONResponse(content=books)
+                else:
+                    raise HTTPException(status_code=400,detail="bad request")
     except:
         return {}
-async def check_as_publish(id:int,book:PublishedBook,db=Depends(create_db)):
+    
+async def check_as_publish(id:int,book:PublishedBook):
     try:
-        books=await db.execute("UPDATE books SET ispublished=%s WHERE id=%s",(id,book.ispublished))
-        if books:
-            return JSONResponse(content=books)
-        else:
-            raise HTTPException(status_code=400,detail="bad request")
+        async with pool.connection() as conn:
+            async with conn.cursor() as curs:
+                books=await curs.execute("UPDATE books SET ispublished=%s WHERE id=%s",(id,book.ispublished))
+                if books:
+                    return JSONResponse(content=books)
+                else:
+                    raise HTTPException(status_code=400,detail="bad request")
     except:
         return {}
     
