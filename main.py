5351ece3fd26d7b4a9e7aa52a113408f2b382af8
asliddin <aslnabiev2002@gmail.com>
2025-05-20 14:05:55 +0500

    return index.html

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter,  WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+from .package.controller import middleware
 from fastapi.middleware.cors import CORSMiddleware
 from .package.controller.user_router import router as u_router
 from .package.controller.book_router import router as b_router
@@ -9,6 +11,7 @@ from os import getenv
 
 
 app=FastAPI()
+router = APIRouter(prefix = "/api", tags = ["auth"])
 def async_get_database():
     conn=async_get_db()
     try:
@@ -28,6 +31,39 @@ app.add_middleware(
 app.include_router(u_router)
 app.include_router(b_router)
 app.include_router(a_router)
+
+
+manager = middleware.ConnectionManager()
+
+@router.get('/')
+async def get():
+    return HTMLResponse("index.html")
+
+@router.websocket('/ws/{client_id}')
+async def web_socket(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.client_send_message(f'You wrote {data}', websocket)
+            await manager.broadcast(f'Client #{client_id} says : {data}')
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client {client_id} left the chat")
+        
+        
+    
+    
+
+
+
+
+
+
+
+
+
+    
 
 
 
